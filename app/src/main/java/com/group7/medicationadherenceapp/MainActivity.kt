12ea02886acacf7180e02a0d92d96f43f75dd615
AlexFTV <144455302+Drago361@users.noreply.github.com>
2025-10-26 19:07:57 +0100

    feat: Remove unused imports

--- app/src/main/java/com/group7/medicationadherenceapp/MainActivity.kt
+++ app/src/main/java/com/group7/medicationadherenceapp/MainActivity.kt
@@ -15,7 +15,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+//import androidx.compose.ui.unit.sp
 import androidx.compose.ui.graphics.vector.ImageVector // Specific UI/Graphics type
 
 // KOTLIN AND JAVA
